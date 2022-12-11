84e6ce0d8d6e94184e1044c3f89850531e5d163d
i-708 <naoya.inoue0830@gmail.com>
2022-12-11 18:41:33 +0900

    fix:basePathの変更

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+const prod = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  basePath: prod ? '/portfolio' : '',
 }
 
 module.exports = nextConfig
