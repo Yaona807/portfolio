11aa996d820cab40dcba66d8e0d614f6f25d5b5f
i-708 <naoya.inoue0830@gmail.com>
2022-12-14 00:04:03 +0900

    improvement：モーダルを閉じられるように変更

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -7,54 +7,67 @@ export function Skills() {
     {
       skill_name: "JavaScript",
       src: "/JavaScript.svg",
+      content: "",
     },
     {
       skill_name: "Python",
       src: "/Python.svg",
+      content: "",
     },
     {
       skill_name: "TypeScript",
       src: "/TypeScript.svg",
+      content: "",
     },
     {
       skill_name: "Node.js",
       src: "/Nodejs.svg",
+      content: "",
     },
     {
       skill_name: "Riot.js",
       src: "/Riotjs.svg",
+      content: "",
     },
     {
       skill_name: "Next.js",
       src: "/Nextjs.svg",
+      content: "",
     },
     {
       skill_name: "CSS",
       src: "/CSS.svg",
+      content: "",
     },
     {
       skill_name: "HTML",
       src: "/HTML.svg",
+      content: "",
     },
     {
       skill_name: "Sass",
       src: "/Sass.svg",
+      content: "",
     },
     {
       skill_name: "React.js",
       src: "/Reactjs.svg",
+      content: "",
     },
     {
       skill_name: "C",
       src: "/C.svg",
+      content: "",
     },
     {
       skill_name: "Java",
       src: "/Java.svg",
+      content: "",
     },
     {
       skill_name: "MySQL",
       src: "/MySQL.svg",
+      content: "",
     },
   ];
   return (
